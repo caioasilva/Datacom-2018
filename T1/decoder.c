d9f97418b342244ae739bdbb80eebfb4eb5a1486
Luís Felipe Tomazini <lftomazini@hotmail.com>
2018-05-13 20:10:25 -0300

    nrz decoding working

---
+++ T1/decoder.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+/**
+* Allocates memory for the output variables
+* 
+* @param out string in binary
+* @param len length of the string to be converted
+* @param the length of the binary string
+*
+* @return 0 if succeeds or -1 in case of error
+*/
+int32_t mem_alloc(char **out, uint64_t len, uint32_t str_len) {
+    if(len == 0) {
+        printf("Length argument is zero\n");
+        return (-1);
+    }
+
+    (*out) = malloc(str_len + 1);
+    if((*out) == NULL) {
+        printf("Can't allocate binary string: %s\n", strerror(errno));
+        return (-1);
+    }
+
+    if(memset((*out), 0, (str_len)) == NULL) {
+        printf("Can't initialize memory to zero: %s\n", strerror(errno));
+        return (-1);
+    }
+
+    return 0;
+}
+
+int bin2dec (int num) {
+    int  decimal_val = 0, base = 1, rem;
+
+    while (num > 0) {
+        rem = num % 10;
+        decimal_val = decimal_val + rem * base;
+        num = num / 10 ;
+        base = base * 2;
+    }
+
+    return decimal_val;
+}
+
+/**
+* Converts a sequence of ascii characters into its correpondents binary
+* 
+* @param input string to be converted
+* @param out string in binary
+* @param len length of the string to be converted
+*
+* @return the length of the new binary string or -1 in case of error
+*/
+int32_t ascii_to_binary(char *input, char **out, uint64_t len) {
+    uint32_t i;
+    int32_t rtn;
+    uint32_t str_len = len * 8;
+
+    if((rtn = mem_alloc(out, len, str_len)) == -1){
+        return -1;
+    }
+
+    for(i = 0; i < len; i++) {
+        unsigned char ch = input[i];
+        char *o = &(*out)[8 * i];
+        unsigned char b;
+
+        for (b = 0x80; b; b >>= 1)
+            *o++ = ch & b ? '1' : '0';
+    }
+
+    (*out)[str_len] = '\0';
+
+    return (str_len);
+}
+
+int32_t binary_to_ascii(char *input, char **out, uint64_t len) {
+    uint32_t i;
+    uint32_t j;
+    int32_t rtn;
+    uint32_t str_len = len / 8;
+
+    if((rtn = mem_alloc(out, len, str_len)) == -1){
+        return -1;
+    }
+
+    for(i = 0; i < str_len; i++) {
+        char *o = &(*out)[i];
+        char subbuff[9];
+        memcpy(subbuff, &input[8 * i], 8);
+        subbuff[8] = '\0';
+        char character = (char) bin2dec(atoi(subbuff));
+
+        *o++ = character;
+    }
+
+    (*out)[str_len] = '\0';
+
+    return (str_len);
+}
+
+int32_t nrz(char* input, char **out, uint64_t len) {
+    int32_t rtrn = 0;
+    char *buffer = NULL;
+    printf("%s\n", input);
+
+    rtrn = binary_to_ascii(input, out, len);
+    if(rtrn < 0) {
+        printf("Can't convert string\n");
+        return (-1);
+    }
+
+    return len;
+}
+
+int32_t manchester(char* input, char **out, uint64_t len) {
+    return len;
+}
+
+int32_t nrzi(char* input, char **out, uint64_t len) {
+    return len;
+}
+
+int32_t _4b5b(char* input, char **out, uint64_t len) {
+    return len;
+}
+
+int main(int argc, char *argv[]) {
+    int32_t rtrn = 0;
+    char *buffer = NULL;
+    char *decoding = NULL;
+
+    if (strcmp(argv[1], "-n") == 0) {
+        nrz(argv[2], &decoding, strlen(argv[2]));
+    } else if (strcmp(argv[1], "-m") == 0) {
+        manchester(argv[2], &decoding, strlen(argv[2]));
+    } else if (strcmp(argv[1], "-i") == 0) {
+        nrzi(argv[2], &decoding, strlen(argv[2]));
+    } else if (strcmp(argv[1], "-f") == 0) {
+        _4b5b(argv[2], &decoding, strlen(argv[2]));
+    }
+
+    printf("dec: %s\n", decoding);
+
+    return (0);
+}
